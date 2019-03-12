d3d94ca92104206753784b0dc19986b3a81f71d7
Keith Nguyen <khoacn@gmail.com>
2019-03-11 21:06:30 -0700

    updated porfolio

--- src/assets/videoscript/video.js
+++ src/assets/videoscript/video.js
@@ -15,3 +15,21 @@ $(document).ready(function(){
         $("#Video").attr('src', url);
     });
 });
+
+$(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#catVideo").attr('src');
+    
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#catModal").on('hide.bs.modal', function(){
+        $("#catVideo").attr('src', '');
+    });
+    
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#catModal").on('show.bs.modal', function(){
+        $("#catVideo").attr('src', url);
+    });
+});
